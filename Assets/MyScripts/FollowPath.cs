0253aa88ba93e129e547f064fbc2b7f113b6e045
Aluno <aluno@LI.LOCAL>
2022-05-27 20:15:21 -0300

    Initial commit

---
+++ Assets/MyScripts/FollowPath.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class FollowPath : MonoBehaviour
+{
+    Transform goal;
+    float speed = 5.0f;
+    float accuracy = 1.0f;
+    float rotSpeed = 2.0f;
+    public GameObject wpManager;
+    GameObject[] wps;
+    GameObject currentNode;
+    int currentWP = 0;
+    Graph g;
+
+    void Start()
+    {
+        wps = wpManager.GetComponent<WPManager>().waypoints;
+        g = wpManager.GetComponent<WPManager>().graph;
+        currentNode = wps[0];
+    }
+    public void GoToHeli()
+    {
+        //Passa ao método os pontos atuais e alvo para mover o agente [1]
+        g.AStar(currentNode, wps[1]);
+        //Zera o contador de movimento
+        currentWP = 0;
+    }
+    //Método para se mover ao ponto ruina
+    public void GoToRuin()
+    {
+        //Passa ao método os pontos atuais e alvo para mover o agente [6]
+        g.AStar(currentNode, wps[6]);
+        //Zera o contador de movimento
+        currentWP = 0;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (g.getPathLength() == 0 || currentWP == g.getPathLength())
+            return;
+
+        //O nó que estará mais próximo neste momento
+        currentNode = g.getPathPoint(currentWP);
+        //se estivermos mais próximo bastante do nó o tanque se moverá para o próximo
+        if (Vector3.Distance(
+        g.getPathPoint(currentWP).transform.position,
+        transform.position) < accuracy)
+        {
+            currentWP++;
+        }
+
+        if (currentWP < g.getPathLength())
+        {
+            //Define proximo ponto alvo do movimento
+            goal = g.getPathPoint(currentWP).transform;
+            //Aloca próximo ponto em um vetor
+            Vector3 lookAtGoal = new Vector3(goal.position.x,
+            this.transform.position.y,
+            goal.position.z);
+            //Utiliza o vetor para rotacionar em direção ao alvo
+            Vector3 direction = lookAtGoal - this.transform.position;
+            //Rotaciona e move o objeto
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation,
+            Quaternion.LookRotation(direction),
+            Time.deltaTime * rotSpeed);
+        }
+    }
+}
